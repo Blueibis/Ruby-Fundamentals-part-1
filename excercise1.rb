ce2462f3969ab8a2107e7afedd877f3f1ee6c2fe
Blueibis <jennie.li.2011@gmail.com>
2017-05-15 13:05:22 -0400

    Excercise 2

--- excercise1.rb
+++ excercise1.rb
@@ -1,3 +1,4 @@
 puts 2
 puts 3
 puts 2 + 3
+puts "Hello\rStart over world"
---
+++ excercise2.rb
@@ -0,0 +1,9 @@
+#1)Good is better than average so a good tip must be between 10 and 15 percent. So we'll say 13 percent.
+good_tip = 0.13
+puts good_tip * 55
+#2)
+puts "string" + 5.to_s
+#3)
+puts "My gramps lost #{45628 * 7839} teeth last year."
+#4)false && true || !false == true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
